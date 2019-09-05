a6f00323ade5939b494f72bc78031138007186ac
schachr <schachr@github.com>
2019-09-05 18:22:54 +0200

    Fix database.yml creation

--- container-files/bootstrap.sh
+++ container-files/bootstrap.sh
@@ -2,32 +2,37 @@
 
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.3-p551/bin
 
-# Apply timezone settings
-ln -f -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo 'snorby: &snorby' > /usr/local/src/snorby/config/database.yml
+echo "  adapter: mysql" >> /usr/local/src/snorby/config/database.yml
+echo "  username: $DB_USER" >> /usr/local/src/snorby/config/database.yml
+echo "  password: \"$DB_PASS\"" >> /usr/local/src/snorby/config/database.yml
+echo "  host: $DB_ADDRESS" >> /usr/local/src/snorby/config/database.yml
+echo "" >> /usr/local/src/snorby/config/database.yml
+echo "production:" >> /usr/local/src/snorby/config/database.yml
+echo "  database: snorby" >> /usr/local/src/snorby/config/database.yml
+echo '  <<: *snorby' >> /usr/local/src/snorby/config/database.yml
+echo "" >> /usr/local/src/snorby/config/database.yml
+
 
 # Prepare Database if it doesn't exist
 export MYSQL_PWD=$DB_PASS
-STATUS=`mysqlshow -u $DB_USER -h $DB_ADDRESS $DB_DATABASE`
-if [[ $STATUS != "$DB_DATABASE" ]]; then
-    mysql -u $DB_USER -h $DB_ADDRESS -e "CREATE DATABASE $DB_DATABASE"
-    mysql -u $DB_USER -h $DB_ADDRESS -e "GRANT ALL ON $DB_DATABASE.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASS'"
+STATUS=$( echo "show databases" | mysql -u $DB_USER -h $DB_ADDRESS | fgrep $DB_DATABASE )
+if [[ $STATUS != "snorby" ]]; then
+    mysql -u $DB_USER -h $DB_ADDRESS -e "CREATE DATABASE snorby"
+    mysql -u $DB_USER -h $DB_ADDRESS -e "GRANT ALL ON snorby.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASS'"
     mysql -u $DB_USER -h $DB_ADDRESS -e "flush privileges"
     cd /usr/local/src/snorby
     bundle install
-    sed -i 's/$DB_ADDRESS/'$DB_ADDRESS'/g' /usr/local/src/snorby/config/database.yml
-    sed -i 's/$DB_USER/'$DB_USER'/g' /usr/local/src/snorby/config/database.yml
-    sed -i 's/$DB_PASS/'$DB_PASS'/g' /usr/local/src/snorby/config/database.yml
-    sed -i 's/$DB_DATABASE/'$DB_DATABASE'/g' /usr/local/src/snorby/config/database.yml
     bundle exec rake snorby:setup
 fi
 
 # Download latest rules when provided with valid Oinkcode from snort.org
 if [ $OINKCODE != "community" ]; then
-    wget -O /tmp/rules.tar.gz https://www.snort.org/rules/snortrules-snapshot-29110.tar.gz?oinkcode=$OINKCODE
-    rm -rf /etc/snort/rules
-    mkdir -p /etc/snort/rules
-    tar zxvf /tmp/rules.tar.gz -C /etc/snort/rules --strip-components=1
-    rm -f /tmp/rules.tar.gz
+    wget -O /tmp/rules.tar.gz https://www.snort.org/rules/snortrules-snapshot-2970.tar.gz?oinkcode=$OINKCODE
+    rm -rf      /etc/snort/rules
+    mkdir -p    /etc/snort/rules
+    tar zxvf    /tmp/rules.tar.gz -C /etc/snort/rules --strip-components=1
+    rm -f       /tmp/rules.tar.gz
 fi
 
 # User params
@@ -54,6 +59,7 @@ log() {
 # Launch Snorby
 log $SNORBY_CMD
 cd /usr/local/src/snorby && $SNORBY_CMD
+
 # Exit immidiately in case of any errors or when we have interactive terminal
 if [[ $? != 0 ]] || test -t 0; then exit $?; fi
 log "Snorby started with $SNORBY_CONFIG config" && log
